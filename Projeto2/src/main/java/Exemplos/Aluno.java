fed2aa3cff01df1a96a0e123f5e60bab06d027ee
Heitor Messias Gomes <heitormessias13@gmail.com>
2024-10-04 20:39:06 -0300

    Adicionei aluno

---
+++ Projeto2/src/main/java/Exemplos/Aluno.java
@@ -0,0 +1,18 @@
+
+package Exemplo;
+
+public class Aluno {
+    
+    private String codigo;
+    private String nome;
+    private String endereco;
+    
+    Aluno(int codigo, String nome){
+        this.codigo = c;
+        this.nome = n;
+      
+    }
+    
+    
+    
+}
